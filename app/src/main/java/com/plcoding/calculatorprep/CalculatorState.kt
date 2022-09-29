0219345510943f5f6a14d93abe71648c0c916ad6
Ved Ratan <vedratan8@gmail.com>
2022-09-29 13:49:47 +0530

    calculator app updated

---
+++ app/src/main/java/com/plcoding/calculatorprep/CalculatorState.kt
@@ -0,0 +1,8 @@
+package com.plcoding.calculatorprep
+
+data class CalculatorState(
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null,
+    var  count: Int  = 0
+)
